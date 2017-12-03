a5393e26278bf6c582ba5fe6d41b2975a9a63ee0
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-03 18:53:27 -0500

    Delete button

--- pages/show_account.php
+++ pages/show_account.php
@@ -19,12 +19,12 @@
 
 <?php
 //this is how you print something  $data contains the record that was selected on the table.
-
 print_r($data);
-
-
 ?>
 
+<form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+    <button type="submit" form="form1" value="delete">Delete</button>
+</form>
 
 <script src="js/scripts.js"></script>
 </body>
