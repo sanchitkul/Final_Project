79721239731d5135aa282da287d0d990157e3a1c
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-03 18:55:42 -0500

    Controller changes

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -65,6 +65,13 @@ class accountsController extends http\controller
         self::getTemplate('edit_account', $record);
 
     }
+    public static function delete()
+    {
+        $record = accounts::findOne($_REQUEST['id']);
+        $record->delete();
+        header("Location: index.php?page=accounts&action=all");
+    }
+
 
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
