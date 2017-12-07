379d11cd59bbd45f6162009afb6c718af3f255cc
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-07 15:01:58 -0500

    Added login in account controller

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -5,12 +5,9 @@
  * Date: 11/27/17
  * Time: 5:32 PM
  */
-
-
 //each page extends controller and the index.php?page=tasks causes the controller to be called
 class accountsController extends http\controller
 {
-
     //each method in the controller is named an action.
     //to call the show function the url is index.php?page=task&action=show
     public static function show()
@@ -18,21 +15,21 @@ class accountsController extends http\controller
         $record = accounts::findOne($_REQUEST['id']);
         self::getTemplate('show_account', $record);
     }
-
     //to call the show function the url is index.php?page=task&action=list_task
-
     public static function all()
     {
-
         $records = accounts::findAll();
         self::getTemplate('all_accounts', $records);
-
     }
+    public static function one()
+    {
+        $records = accounts::findOne(10);
+        self::getTemplate('one_accounts',$records);
+    }
+
     //to call the show function the url is called with a post to: index.php?page=task&action=create
     //this is a function to create new tasks
-
     //you should check the notes on the project posted in moodle for how to use active record here
-
     //this is to register an account i.e. insert a new account
     public static function register()
     {
@@ -50,20 +47,23 @@ class accountsController extends http\controller
         $record->password = "12345";
         $record->save();
     }
-
     //this is the function to save the user the user profile
     public static function store()
     {
+        $user = accounts::findUSerbyEmail($REQUEST['email']);
+        if (user = FALSE)
+        {
+            $user
+        }
+
         print_r($_POST);
 
-    }
 
+    }
     public static function edit()
     {
         $record = accounts::findOne($_REQUEST['id']);
-
         self::getTemplate('edit_account', $record);
-
     }
     public static function delete()
     {
@@ -71,8 +71,6 @@ class accountsController extends http\controller
         $record->delete();
         header("Location: index.php?page=accounts&action=all");
     }
-
-
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
     {
@@ -82,9 +80,25 @@ class accountsController extends http\controller
         //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
         //after you login you can use the header function to forward the user to a page that displays their tasks.
         //        $record = accounts::findUser($_POST['uname']);
-
-        print_r($_POST);
-
+        $user = accounts::findUserbyEmail($_REQUEST['email']);
+        if ($user == FALSE)
+        {
+            echo 'user not found';
+        }
+        else
+        {
+            if($user->checkPassword($_POST['password'])== TRUE )
+            {
+                echo 'login';
+                session_start();
+                $SESSION["userID"] = $user->id;
+                print_r($_SESSION);
+             }
+             else
+             {
+                echo 'password does not match';
+             }   
+        }
     }
-
 }
+
