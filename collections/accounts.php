e4c1141d639a2dd8a7b4abe74da18e66131b3dbf
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-13 17:10:02 -0500

    findbyusername added

--- collections/accounts.php
+++ collections/accounts.php
@@ -1,12 +1,22 @@
 <?php
-
 class accounts extends \database\collection
 {
     protected static $modelName = 'account';
+    public static  function findUserbyUsername($login) {
+    $tableName = get_called_class();
+    $sql = 'SELECT * FROM ' .$tableName . ' WHERE email ="' . $login.'"';
+    $recordsSet = self::getResults($sql);
+    if (is_null($recordsSet)) 
+    {
+       echo "False";
+       return FALSE;
+    } 
+    else 
+    {
+       echo "True";
+       return $recordsSet[0];
+    }
 
-    //This is the function to write to find user by ID for login.
-    //Don't forget to return the object see findOne in the collection class
-    public static  function findUserbyID($userid) {}
+    }
 }
-
 ?>
