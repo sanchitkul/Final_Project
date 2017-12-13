9e996556b40f054d9702d54926ee8fdaf882b5c2
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-13 17:07:18 -0500

    Added findtaskbyid

--- collections/todos.php
+++ collections/todos.php
@@ -1,12 +1,13 @@
 <?php
-
 class todos extends database\collection
 {
     protected static $modelName = 'todo';
-
-    //This is the function to write to find tasks by user ID for listing on their homepage.
-    //Don't forget to return the record set see findAll in the collection class
-    public static  function findTasksbyID($userid) {}
+    
+    public static  function findTasksbyID($userid) {
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid ="' . $userid.'"';
+        $recordsSet = self::getResults($sql);
+        return $recordsSet;
+    }
 }
-
 ?>
