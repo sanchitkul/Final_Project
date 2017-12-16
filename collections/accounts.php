a50bff2b6c9b0a5a0bc95c32e7d76af321563c02
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-16 15:10:09 -0500

    silly mistake

--- collections/accounts.php
+++ collections/accounts.php
@@ -2,7 +2,7 @@
 class accounts extends \database\collection
 {
     protected static $modelName = 'account';
-    public static  function findUserbyUsername($login) {
+    public static  function findUserbyEmail($login) {
     $tableName = get_called_class();
     $sql = 'SELECT * FROM ' .$tableName . ' WHERE email ="' . $login.'"';
     $recordsSet = self::getResults($sql);
@@ -16,7 +16,6 @@ class accounts extends \database\collection
        echo "True";
        return $recordsSet[0];
     }
-
     }
 }
 ?>
