1f6e833c9c6eed65e79a35296e8247d9d100407e
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-17 00:54:54 -0500

    updated

--- collections/accounts.php
+++ collections/accounts.php
@@ -2,20 +2,18 @@
 class accounts extends \database\collection
 {
     protected static $modelName = 'account';
-    public static  function findUserbyEmail($login) {
-    $tableName = get_called_class();
-    $sql = 'SELECT * FROM ' .$tableName . ' WHERE email ="' . $login.'"';
-    $recordsSet = self::getResults($sql);
-    if (is_null($recordsSet)) 
+    //This is the function to write to find user by ID for login.
+    //Don't forget to return the object see findOne in the collection class
+    public static function findUserbyEmail($email)
     {
-       echo "False";
-       return FALSE;
-    } 
-    else 
-    {
-       echo "True";
-       return $recordsSet[0];
-    }
+            $tableName = get_called_class();
+            $sql = 'SELECT * FROM ' . $tableName . ' WHERE email = ?';
+         //grab the only record for find one and return as an object
+            $recordsSet = self::getResults($sql, $email);
+            if (is_null($recordsSet)) {
+                return FALSE;
+            } else {
+                return $recordsSet[0];
+            }
     }
 }
-?>
