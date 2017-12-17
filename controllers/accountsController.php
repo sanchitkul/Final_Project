e01e93de45a5635b9c10a30bf493335496f20cba
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-12-16 20:06:59 -0500

    Login successful

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -28,6 +28,8 @@ class accountsController extends http\controller
     {
         self::getTemplate('new_user');
     }
+
+    
     //this is to register an account i.e. insert a new account
   
     public static function register()
@@ -36,28 +38,37 @@ class accountsController extends http\controller
         //USE THE ABOVE TO SEE HOW TO USE Bcrypt
         self::getTemplate('register');
     }
+    
+    public static function show_profile()
+    {
+        session_start();
+        $record = accounts::findOne($_SESSION['userID']);
+        self::getTemplate('show_account', $record);
+    }
+
+
     //this is the function to save the user the new user for registration
     public static function store()
     {
         $user = accounts::findUserbyEmail($_REQUEST['email']);
         if ($user == FALSE) {
-            $user = new account();
-            $user->email = $_POST['email'];
-            $user->fname = $_POST['fname'];
-            $user->lname = $_POST['lname'];
-            $user->phone = $_POST['phone'];
-            $user->birthday = $_POST['birthday'];
-            $user->gender = $_POST['gender'];
+            $record = new account();
+            $record->email = $_POST['email'];
+            $record->fname = $_POST['fname'];
+            $record->lname = $_POST['lname'];
+            $record->phone = $_POST['phone'];
+            $record->birthday = $_POST['birthday'];
+            $record->gender = $_POST['gender'];
             //$user->password = $_POST['password'];
             //this creates the password
             //this is a mistake you can fix...
             //Turn the set password function into a static method on a utility class.
             $user->password = $user->setPassword($_POST['password']);
-            $user->save();
+            $record->save();
             //you may want to send the person to a
             // login page or create a session and log them in
             // and then send them to the task list page and a link to create tasks
-            header("Location: index.php?page=accounts&action=all");
+            header('Location: index.php');
         } else {
             //You can make a template for errors called error.php
             // and load the template here with the error you want to show.
@@ -68,22 +79,11 @@ class accountsController extends http\controller
     }
     public static function edit()
     {
-        $record = accounts::findOne($_REQUEST['id']);
+        session_start();
+        $record = accounts::findOne($_SESSION['userID']);
         self::getTemplate('edit_account', $record);
     }
-//this is used to save the update form data
-    public static function save() {
-        $user = accounts::findOne($_REQUEST['id']);
-        $user->email = $_POST['email'];
-        $user->fname = $_POST['fname'];
-        $user->lname = $_POST['lname'];
-        $user->phone = $_POST['phone'];
-        $user->birthday = $_POST['birthday'];
-        $user->gender = $_POST['gender'];
-        $user->save();
-        header("Location: index.php?page=accounts&action=all");
-    }
-    public static function delete() {
+       public static function delete() {
         $record = accounts::findOne($_REQUEST['id']);
         $record->delete();
         header("Location: index.php?page=accounts&action=all");
@@ -96,23 +96,25 @@ class accountsController extends http\controller
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
     {
-        //you will need to fix this so we can find users username.  YOu should add this method findUser to the accounts collection
-        //when you add the method you need to look at my find one, you need to return the user object.
-        //then you need to check the password and create the session if the password matches.
-        //you might want to add something that handles if the password is invalid, you could add a page template and direct to that
-        //after you login you can use the header function to forward the user to a page that displays their tasks.
-        //        $record = accounts::findUser($_POST['email']);
-        $user = accounts::findUserbyEmail($_REQUEST['email']);
-        if ($user == FALSE) {
+        $record= new account();
+        $record = accounts::findUserbyEmail($_POST['email']);
+        if ($record == FALSE)
+         {
             echo 'user not found';
-        } else {
-            if($user->checkPassword($_POST['password']) == TRUE) {
-                echo 'login';
+         }
+         else 
+         {
+            if($record->checkPassword($_POST['password']) == TRUE) {
+                //echo 'login';
                 session_start();
-                $_SESSION["userID"] = $user->id;
+                $_SESSION["userID"] = $record->id;
+                $_SESSION["userEmail"] = $record->email;
                 //forward the user to the show all todos page
                 print_r($_SESSION);
-            } else {
+                header('Location: index.php?page=tasks&action=allOneUser&id='.$record->id);
+            } 
+            else 
+            {
                 echo 'password does not match';
             }
         }
